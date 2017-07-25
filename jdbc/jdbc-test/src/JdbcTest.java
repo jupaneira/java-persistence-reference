232fe8b735ffa880c0300514a796984771cdc0f2
Juan Pablo Neira <JuanPablo.NeiraCeballos@wnco.com>
2017-07-25 15:11:15 -0500

    First JDBC example

---
+++ jdbc/jdbc-test/src/JdbcTest.java
@@ -0,0 +1,41 @@
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class JdbcTest {
+
+	public static void main(String[] args) {
+
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+
+		String dbUrl = "jdbc:mysql://localhost:3306/demo?useSSL=false";
+		String user = "student";
+		String pass = "student";
+
+		try {
+			// 1. Get a connection to database
+			myConn = DriverManager.getConnection(dbUrl, user, pass);
+
+			// 2. Create a statement
+			myStmt = myConn.createStatement();
+
+			// 3. Execute SQL Query
+			myRs = myStmt.executeQuery("select * from employees");
+			
+			//4. Process the ResultSet
+			while(myRs.next()){
+				System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name"));
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
