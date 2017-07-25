db1bdc9b3212ec6dbe613807fa5eacaf5f4bfb75
Juan Pablo Neira <JuanPablo.NeiraCeballos@wnco.com>
2017-07-25 15:54:37 -0500

    JDBC Delete Query Example

---
+++ jdbc/jdbc-test/src/JdbcDeleteQuery.java
@@ -0,0 +1,57 @@
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class JdbcDeleteQuery {
+
+	public static void main(String[] args) throws SQLException {
+
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+
+		String dbUrl = "jdbc:mysql://localhost:3306/demo?useSSL=false";
+		String user = "student";
+		String pass = "student";
+
+		try {
+			// 1. Get a connection to database
+			myConn = DriverManager.getConnection(dbUrl, user, pass);
+
+			// 2. Create a statement
+			myStmt = myConn.createStatement();
+
+			// 3. Execute SQL Query
+			System.out.println("\nDELETING THE EMPLOYEE: John Doe\n");
+
+			int rowsAffected = myStmt
+					.executeUpdate("delete from employees where last_name='Doe' and first_name='John'");
+
+			// 4. Process the ResultSet
+			myRs = myStmt.executeQuery(
+					"select last_name, first_name, email from employees where last_name='Doe' and first_name='John'");
+			while (myRs.next()) {
+				System.out.println("->" + myRs.getString("last_name") + ", " + myRs.getString("first_name"));
+			}
+
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		} finally {
+			if (myRs != null) {
+				myRs.close();
+			}
+
+			if (myStmt != null) {
+				myStmt.close();
+			}
+
+			if (myConn != null) {
+				myConn.close();
+			}
+		}
+
+	}
+
+}
